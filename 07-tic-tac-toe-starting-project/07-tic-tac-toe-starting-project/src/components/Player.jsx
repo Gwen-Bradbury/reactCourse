33ebe97856cc9cae40c605b94b1dabf3d4a0460a
Gwen Bradbury <charmed_89@msn.com>
2024-06-10 13:21:40 +0100

    react essentials: advanced concepts - component instances work in isolation

--- 07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -6,7 +6,11 @@ export default function Player({ name, symbol }) {
   function handleEditClick() {
     setIsEditing(true);
   }
-
+  // this function is being outputted twice in App.jsx
+  // when clicking player 1's edit button the input field appears but player 2's input stays hidden and vice versa
+  // this is because whenever you reuse a component react creates a new isolated instance
+  // so player 1 and player 2 will work completely independent of each other even though they're using the same component
+  // so if the state changes when <Player /> gets called the first time the second time it gets called it won't care about that - it doesn't even know about it
   return (
     <li>
       <span className="player">
