c3cbdf46dc798a4dc67461b9a447efccf9a14009
Gwen Bradbury <charmed_89@msn.com>
2024-06-18 21:12:37 +0100

    styling react components: styling with inline styles

--- 01-css-starting-project/01-css-starting-project/src/components/Header.jsx
+++ 01-css-starting-project/01-css-starting-project/src/components/Header.jsx
@@ -6,7 +6,18 @@ export default function Header() {
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      {/* inline styling is passed through as an object using the style tag and key value pair */}
+      {/* it also only affects the element you add it too */}
+      <p
+        style={{
+          color: "red",
+          // you cant have two words and a dash as a naming convention so you either have to wrap it as a string or use camel case
+          // 'text-align': 'center'
+          textAlign: "center",
+        }}
+      >
+        A community of artists and art-lovers.
+      </p>
     </header>
   );
 }
