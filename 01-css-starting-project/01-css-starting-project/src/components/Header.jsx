6d82ec56b06693705b448f01f6e36bc85eb48156
Gwen Bradbury <charmed_89@msn.com>
2024-06-21 20:32:30 +0100

    styling react components: scoping css rules with css modules

--- 01-css-starting-project/01-css-starting-project/src/components/Header.jsx
+++ 01-css-starting-project/01-css-starting-project/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import logo from "../assets/logo.png";
-import "./Header.css";
+import classes from "./Header.module.css";
 
 export default function Header() {
   return (
@@ -9,12 +9,16 @@ export default function Header() {
       {/* inline styling is passed through as an object using the style tag and key value pair */}
       {/* it also only affects the element you add it too */}
       <p
-        style={{
-          color: "red",
-          // you cant have two words and a dash as a naming convention so you either have to wrap it as a string or use camel case
-          // 'text-align': 'center'
-          textAlign: "center",
-        }}
+        // style={{
+        //   color: "red",
+        //   // you cant have two words and a dash as a naming convention so you either have to wrap it as a string or use camel case
+        //   // 'text-align': 'center'
+        //   textAlign: "center",
+        // }}
+        // when using css classes and modules the css is scoped to the file it's being imported into
+        // this css is stored as .paragraph in the Header.module.css file which is imported at the top
+        // classes is what we called the import. It could have been named styles or anything relative
+        className={classes.paragraph}
       >
         A community of artists and art-lovers.
       </p>
