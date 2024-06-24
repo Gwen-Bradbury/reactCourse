4adf967efb7aa29c318d109923fa763b9c306c92
Gwen Bradbury <charmed_89@msn.com>
2024-06-24 21:06:30 +0100

    styling react components: creating flexible components with styled components

--- 01-css-starting-project/01-css-starting-project/src/components/AuthInputs.jsx
+++ 01-css-starting-project/01-css-starting-project/src/components/AuthInputs.jsx
@@ -23,6 +23,27 @@ const controlContainer = styled.div`
   margin-bottom: 1.5rem;
 `;
 
+const Label = styled.label`
+  display: block;
+  margin-bottom: 0.5rem;
+  font-size: 0.75rem;
+  font-weight: 700;
+  letter-spacing: 0.1em;
+  text-transform: uppercase;
+  color: #6b7280;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  padding: 0.75rem 1rem;
+  line-height: 1.5;
+  background-color: #d1d5db;
+  color: #374151;
+  border: 1px solid transparent;
+  border-radius: 0.25rem;
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+`;
+
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState("");
   const [enteredPassword, setEnteredPassword] = useState("");
@@ -50,10 +71,22 @@ export default function AuthInputs() {
         <p>
           {/* if an element already has a class and you want to add another conditional class you can use backticks and the ${} syntax to achive it */}
           {/* label is the hardcoded class and is the dynamically, conditionally injected class */}
-          <label className={`label ${emailNotValid ? "invalid" : ""}`}>
+          {/* <label className={`label ${emailNotValid ? "invalid" : ""}`}>
             Email
-          </label>
-          <input
+          </label> */}
+          {/* using the styled component const Label from above */}
+          {/* the styled Label const above is actually still a <label></label> tag thats been styled, so you can also still pass dynamic jsx code into it */}
+          <Label className={`label ${emailNotValid ? "invalid" : ""}`}>
+            Email
+          </Label>
+          {/* the input before we used the styled Input created above */}
+          {/* <input
+            type="email"
+            style={{ backgroundColor: emailNotValid ? "#fed2d2" : "#d1d5db" }}
+            // className={emailNotValid ? "invalid" : undefined}
+            onChange={(event) => handleInputChange("email", event.target.value)}
+          /> */}
+          <Input
             type="email"
             style={{ backgroundColor: emailNotValid ? "#fed2d2" : "#d1d5db" }}
             // className={emailNotValid ? "invalid" : undefined}
@@ -61,10 +94,10 @@ export default function AuthInputs() {
           />
         </p>
         <p>
-          <label className={`label ${emailNotValid ? "invalid" : ""}`}>
+          <Label className={`label ${emailNotValid ? "invalid" : ""}`}>
             Password
-          </label>
-          <input
+          </Label>
+          <Input
             type="password"
             className={passwordNotValid ? "invalid" : undefined}
             onChange={(event) =>
