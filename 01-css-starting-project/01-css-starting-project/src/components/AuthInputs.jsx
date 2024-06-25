606794c0cb123d9716a956ff5b2787c3bb022cd2
Gwen Bradbury <charmed_89@msn.com>
2024-06-25 17:44:24 +0100

    styling react components: dynamic and conditional styling with styled components

--- 01-css-starting-project/01-css-starting-project/src/components/AuthInputs.jsx
+++ 01-css-starting-project/01-css-starting-project/src/components/AuthInputs.jsx
@@ -30,19 +30,24 @@ const Label = styled.label`
   font-weight: 700;
   letter-spacing: 0.1em;
   text-transform: uppercase;
-  color: #6b7280;
+  // invalid is the emailNotValid prop that we sent in in the <Label></Label>
+  // this sets the colour dynamically so it can be used as a conditional on the Email label
+  // if invalid is true set the #f87171 else set the #6b7280
+  color: ${({ $invalid }) => ($invalid ? "#f87171" : "#6b7280")};
 `;
 
 const Input = styled.input`
   width: 100%;
   padding: 0.75rem 1rem;
   line-height: 1.5;
-  background-color: #d1d5db;
-  color: #374151;
-  border: 1px solid transparent;
+  background-color: ${({ $invalid }) => ($invalid ? "#fed2d2" : "#d1d5db")};
+  color: ${({ $invalid }) => ($invalid ? "#ef4444" : "#374151")};
+  border: ${({ $invalid }) =>
+    $invalid ? "1px solid #f73f3f" : "1px solid transparent"};
   border-radius: 0.25rem;
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
 `;
+// border could also be written like > border: 1px solid ${({ invalid }) => (invalid ? "#f73f3f" : "transparent")};
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState("");
@@ -76,9 +81,12 @@ export default function AuthInputs() {
           </label> */}
           {/* using the styled component const Label from above */}
           {/* the styled Label const above is actually still a <label></label> tag thats been styled, so you can also still pass dynamic jsx code into it */}
-          <Label className={`label ${emailNotValid ? "invalid" : ""}`}>
-            Email
-          </Label>
+          {/* <Label className={`label ${emailNotValid ? "invalid" : ""}`}> */}
+          {/* using styled components to conditionally render styles vvv */}
+          {/* we can add a prop with the value emailnotValid which will either be true or false */}
+          {/* and we can use this prop in the styled.label const above to dynamically set the text colour of the <Label> */}
+          {/* prefix any styling props with a $ so they won't conflict with any built in prop names!! */}
+          <Label $invalid={emailNotValid}>Email</Label>
           {/* the input before we used the styled Input created above */}
           {/* <input
             type="email"
@@ -86,20 +94,27 @@ export default function AuthInputs() {
             // className={emailNotValid ? "invalid" : undefined}
             onChange={(event) => handleInputChange("email", event.target.value)}
           /> */}
-          <Input
+          {/* Input before we change to dynamic styling in the const above */}
+          {/* <Input
             type="email"
             style={{ backgroundColor: emailNotValid ? "#fed2d2" : "#d1d5db" }}
+            // className={emailNotValid ? "invalid" : undefined}
+            onChange={(event) => handleInputChange("email", event.target.value)}
+          /> */}
+          <Input
+            type="email"
+            $invalid={emailNotValid}
             // className={emailNotValid ? "invalid" : undefined}
             onChange={(event) => handleInputChange("email", event.target.value)}
           />
         </p>
         <p>
-          <Label className={`label ${emailNotValid ? "invalid" : ""}`}>
-            Password
-          </Label>
+          {/* <Label className={`label ${emailNotValid ? "invalid" : ""}`}> */}
+          <Label $invalid={passwordNotValid}>Password</Label>
           <Input
             type="password"
-            className={passwordNotValid ? "invalid" : undefined}
+            $invalid={passwordNotValid}
+            // className={passwordNotValid ? "invalid" : undefined}
             onChange={(event) =>
               handleInputChange("password", event.target.value)
             }
